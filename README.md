7a364a3110f18017e8d4ecacbc522cc2773139c2
Rui Wang <1102131860@qq.com>
2024-12-01 19:24:43 -0500

    Generate deliverable1.csv and deliveravle2.csv

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 *   Merge the `address`, `latitude`, `longtiude`, and `max responses` together into a csv file
 *   Create a `config.py` and write you own api key into it. You are free to rename the file as what you like.
 *   Change the files' addresses in the `config.py` to what you want to read or write.
+
+## Dec 1st, 2024
+* Ouput four files and directory: `output.json`, `deliverable1.csv`, `deliveravle2.csv` and `original_response/` 
+* Filter the data before storing them into csv
+    * Drop all rows with none solar panel
+    * Flag up those rows where the differences between `Lat` and `center_latitude` or `Lag` and `center_longitude` excedes threshold (temporarily, 0.0005) in deliverable1.csv
+    * Flap up those rows where solar panels are smaller than the minimum threshold or larger than the maximum threshold (temporarily, minimum threshold: 10 and maximum threshold: 2000)
+* The filter results are:
+    * Drop 4 no solar panel rows from original 266 rows, i.e. 262 raws in deliveravle1.csv and deliverable2.csv
+    * 33 rows in deliverable1.csv has abnormal latitude and longitude
+    * 32 rows in deliveravle2.csv has abnoraml solar panels
--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import requests
 import json
-import time
 import os
 from tqdm import tqdm
 try:
-    from config import API_KEY, READ_SRC_CSV, WRITE_RESULT_CSV, WRITE_RESULT_JSON, SAVE_ORIGIN_RESPONSE, ORIGIN_RESPONSE_DIR, LATITUDE, LONGTIDUE, STORE_NAME, ADDRESS
+    from config import API_KEY, READ_SRC_CSV, WRITE_RESULT_CSV_DATA_VERIF,WRITE_RESULT_CSV_OUTPUT_VERIF, \
+        WRITE_RESULT_JSON, SAVE_ORIGIN_RESPONSE, ORIGIN_RESPONSE_DIR, LATITUDE, LONGTIDUE, STORE_NAME, ADDRESS, \
+            LAT_LNG_THRESHOLD, MIN_PANELS, MAX_PANELS
 except ImportError:
     print("Error: Can't find config.py file! Please ensure the config.py file exists and contains the necessary configuration information.")
     exit(1)
@@ -14,16 +15,8 @@ except ImportError:
 # URL for the Google Solar API
 url = "https://solar.googleapis.com/v1/buildingInsights:findClosest"
 
-# Global variables used to count the quality of rows
-total_query_count = 0
-high_quality_count = 0
-medium_quality_count = 0
-no_found_count = 0
-
 # Get and extract required attributes from API response
 def get_building_insights(latitude, longitude, quality="HIGH"):
-    global high_quality_count, medium_quality_count, no_found_count
-
     params = {
         "location.latitude": latitude,
         "location.longitude": longitude,
@@ -40,14 +33,7 @@ def get_building_insights(latitude, longitude, quality="HIGH"):
                 return get_building_insights(latitude, longitude, quality="MEDIUM")
             else:
                 print(f"Error for ({latitude}, {longitude}): {response['error']['message']}")
-                no_found_count += 1
                 return response, None
-
-        # Count the response quality
-        if quality == "HIGH":
-            high_quality_count += 1
-        if quality == "MEDIUM":
-            medium_quality_count += 1
 
         # Extract relevant information for summary
         building_insight = {
@@ -62,14 +48,11 @@ def get_building_insights(latitude, longitude, quality="HIGH"):
 
     except Exception as e:
         print(f"An unexpected error occurred for ({latitude}, {longitude}): {e}")
-        no_found_count += 1
         return None, None
 
 
 # Process the CSV and extract building insights
 def process():
-    global total_query_count
-
     # Save each original response as a separate JSON file
     if SAVE_ORIGIN_RESPONSE:
         os.makedirs(ORIGIN_RESPONSE_DIR, exist_ok=True)
@@ -92,8 +75,6 @@ def process():
 
     # Create tqdm progress bar
     for index, row in tqdm(df.iterrows(), total=len(df), desc="Processing rows"):
-        total_query_count += 1
-
         # Get building insights
         response, building_insight = get_building_insights(row[LATITUDE], row[LONGTIDUE])
 
@@ -116,9 +97,6 @@ def process():
             df.at[index, "YearlyEnergy"] = building_insight["YearlyEnergy"]
             df.at[index, "SolarArea"] = building_insight["SolarArea"]
 
-    # Save the updated DataFrame to a new CSV file
-    df.to_csv(WRITE_RESULT_CSV, index=False)
-
     # Extract only relevant insights for saving to output JSON, ignoring rows with no insights
     subset_column = ["center_latitude", "center_longitude", "NumPanels", "YearlyEnergy", "SolarArea"]
     insights = df.dropna(subset=subset_column)
@@ -127,23 +105,49 @@ def process():
     # Save the extracted building insights to a JSON file
     with open(WRITE_RESULT_JSON, "w") as json_file:
         json.dump(insights_list, json_file, indent=4)
+    
+    # Apply filters
+    # Check latitude and longitude difference thresholds
+    df["DataVerificationFlag"] = (
+        (df["center_latitude"].notnull()) &
+        (df["center_longitude"].notnull()) &
+        (
+            (abs(df[LATITUDE] - df["center_latitude"]) > LAT_LNG_THRESHOLD) |
+            (abs(df[LONGTIDUE] - df["center_longitude"]) > LAT_LNG_THRESHOLD)
+        )
+    ).astype(int)
 
-    print(f"Processing completed! Results saved to {WRITE_RESULT_JSON} and {WRITE_RESULT_CSV}")
+    # Filter out rows with no panels
+    df = df[df["NumPanels"].notnull() & (df["NumPanels"] > 0)]
+
+    # Flag extreme small and large panel values
+    df["OutputVerificationFlag"] = (
+        (df["NumPanels"] < MIN_PANELS) | (df["NumPanels"] > MAX_PANELS)
+    ).astype(int)
+
+    # Count the number of rows and abnormal flags
+    total_rows = len(df)
+    data_verification_flag_count = df["DataVerificationFlag"].sum()
+    output_verification_flag_count = df["OutputVerificationFlag"].sum()
+
+    # Print the counts
+    print(f"Total number of rows after filtering: {total_rows}")
+    print(f"Number of rows with DataVerificationFlag set to 1: {data_verification_flag_count}")
+    print(f"Number of rows with OutputVerificationFlag set to 1: {output_verification_flag_count}")
+
+    # Select different columns to 
+    deliverable_1_columns = [STORE_NAME, ADDRESS, LATITUDE, LONGTIDUE, "center_latitude", "center_longitude", "DataVerificationFlag"]
+    deliveravle_2_columns = [STORE_NAME, ADDRESS, LATITUDE, LONGTIDUE, "NumPanels", "YearlyEnergy", "SolarArea", "OutputVerificationFlag"]
+    deliverable_1_df = df[deliverable_1_columns]
+    deliverable_2_df = df[deliveravle_2_columns]
+
+    # Save the updated DataFrame to new CSV files
+    deliverable_1_df.to_csv(WRITE_RESULT_CSV_DATA_VERIF, index=False)
+    deliverable_2_df.to_csv(WRITE_RESULT_CSV_OUTPUT_VERIF, index=False)
+
+    print(f"Processing completed! Results saved to {WRITE_RESULT_JSON}, {WRITE_RESULT_CSV_DATA_VERIF} and {WRITE_RESULT_CSV_OUTPUT_VERIF}")
 
 
 if __name__ == "__main__":
-    # Start the timer
-    start_time = time.time()
-
     # Process the CSV file and extract building insights
     process()
-
-    # Print the total execution time
-    end_time = time.time()
-    print(f"Total execution time: {end_time - start_time:.2f} seconds")
-
-    # Output the query result
-    print(f"Total queries: {total_query_count}")
-    print(f"High quality queries: {high_quality_count}")
-    print(f"Medium quality queries: {medium_quality_count}")
-    print(f"No response: {no_found_count}")
